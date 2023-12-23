486d202e685528eb4343a58a913f5d501c22f579
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-24 00:05:13 +0700

    add listener to button back

--- app/src/main/java/com/example/suitmedia_android_developer_test/ThirdActivity.kt
+++ app/src/main/java/com/example/suitmedia_android_developer_test/ThirdActivity.kt
@@ -1,11 +1,39 @@
 package com.example.suitmedia_android_developer_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.suitmedia_android_developer_test.databinding.ActivityThirdAcivityBinding
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdAcivityBinding
+
+    private lateinit var btnBack: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third_acivity)
+        binding = ActivityThirdAcivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+        setupComponents()
+        setupListener()
+    }
+
+    private fun initComponents() {
+        btnBack = binding.btnBack
+    }
+
+    private fun setupComponents() {
+
+    }
+
+    private fun setupListener() {
+        btnBack.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }
