3eaa2a7f74c989603b8711ac968a47a4de27e5a3
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-23 14:59:05 +0700

    make seconda and third activity class

---
+++ app/src/main/java/com/example/suitmedia_android_developer_test/ThirdAcivity.kt
@@ -0,0 +1,11 @@
+package com.example.suitmedia_android_developer_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdAcivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_acivity)
+    }
+}
