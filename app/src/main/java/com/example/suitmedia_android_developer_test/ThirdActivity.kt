997d67642bed74720573ad2dcbd8b7dfbef908ad
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-23 19:16:41 +0700

    fix typo activity class name

--- app/src/main/java/com/example/suitmedia_android_developer_test/ThirdActivity.kt
+++ app/src/main/java/com/example/suitmedia_android_developer_test/ThirdActivity.kt
@@ -3,7 +3,7 @@ package com.example.suitmedia_android_developer_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ThirdAcivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_acivity)
