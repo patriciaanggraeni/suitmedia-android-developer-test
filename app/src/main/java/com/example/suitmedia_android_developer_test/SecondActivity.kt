355dade00a33994c19697e91e2e3fb7ceaaf2f6b
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-23 23:45:23 +0700

    add extras for geting data

--- app/src/main/java/com/example/suitmedia_android_developer_test/SecondActivity.kt
+++ app/src/main/java/com/example/suitmedia_android_developer_test/SecondActivity.kt
@@ -2,10 +2,33 @@ package com.example.suitmedia_android_developer_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.suitmedia_android_developer_test.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    private lateinit var username: TextView
+    private lateinit var btnChooseUser: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+        setupComponents()
+    }
+
+    private fun initComponents() {
+        username = binding.name
+        btnChooseUser = binding.btnChooseUser
+    }
+
+    private fun setupComponents() {
+        val username = intent.getStringExtra("username")
+        this.username.text = username
     }
 }
