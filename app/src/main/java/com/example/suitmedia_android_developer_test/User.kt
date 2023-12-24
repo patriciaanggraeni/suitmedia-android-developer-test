57d9c5ee42752612031ebe4575d63d33052deb8f
Patria Anggara <82094818+patriciaanggraeni@users.noreply.github.com>
2023-12-24 07:33:20 +0700

    create user class

---
+++ app/src/main/java/com/example/suitmedia_android_developer_test/User.kt
@@ -0,0 +1,11 @@
+package com.example.suitmedia_android_developer_test
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    val id: Int,
+    val email: String,
+    @SerializedName("first_name") val firstName: String,
+    @SerializedName("last_name") val lastName: String,
+    val avatar: String
+)
